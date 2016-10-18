7ee93b8745719f99205f522e2bae1cb4c6aa656c
Chainatee Tanakulrungson <c.tanakul@gmail.com>
2016-10-18 07:17:37 -0500

    Color union and int (Haven't debug strained line color and edge, haven't done flood-fill)

--- bresenham.cpp
+++ bresenham.cpp
@@ -106,13 +106,30 @@ void put_2points_in_mat(int mat[][2]){
 
 // print array with the column of 2
 int print_array_2(int mat[][2]){
-  int block_number = sizeof(mat);
+  int block_number = sizeof(mat)/(2*sizeof(mat[0][0]));;
   int count = 0;
   while (count < block_number){
     cout << mat[count][0] << "," << mat[count][1] << endl; 
     count++;
   }
 }
+
+//get assignment type from the head of the txt file
+string get_first_line(string filename){
+  //input from file
+  ifstream inFile;
+  inFile.open(filename.c_str());
+  //Check for error
+  if (inFile.fail()){
+    cerr << "Error Opening File" << endl;
+    exit(1);
+  }
+  //get herd file
+  string first_line;
+  inFile >> first_line;
+  return first_line;
+}
+
 
 
 //2.---------------------------------------Bresenham line drawing---------------------------------------------------------------------------------------
@@ -384,7 +401,7 @@ void input_point_from_text(int input[][2],string filename, int size){
 
 
 
-//4-------------------------------------draw polygon from points----------------------------------------------------------
+//4-------------------------------------draw polygon from points(1 polygon)----------------------------------------------------------
 
 //4.1draw polygon from points
 void draw_polygon_line (Mat &image, int mat_points[][2], int mat_size){
@@ -414,7 +431,7 @@ void draw_polygon_line (Mat &image, int mat_points[][2], int mat_size){
 
 }
 
-//5--------------------------------------color the polygon---------------------------------------------------------------
+//5--------------------------------------color the polygon(1 polygon)---------------------------------------------------------------
 
 
 //5.1 --------------return rightmost and leftmost x and y coordinate of a polygon line-------
@@ -469,15 +486,6 @@ Limitpoints get_limitpoints_polygon(int points[][2], int size_point_array){
 void color_polygon_in_limits(Mat &polygon_unfilled, Limitpoints limits){
   //make the horizontal sweepline(y-axis)
   int fill = 0;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(limits.least_y,limits.least_x - 1) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(345,100) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(740,350) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(344,99) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(739,349) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(738,348) << endl;
-  cout << "test   " << polygon_unfilled.at<Vec3b>(741,351) << endl;
-
-
 
   for(int y = limits.least_y; y < limits.most_y ; y++){
     //make the vertical sweepline for each step of horizontal sweepline(x-axis with each x-axis)
@@ -489,7 +497,7 @@ void color_polygon_in_limits(Mat &polygon_unfilled, Limitpoints limits){
       else if((polygon_unfilled.at<Vec3b>(y,x) == Vec3b(0,0,0)) && (fill == 1)){
 	break;
       }
-      if(fill == 1){
+      if(fill == 1 and y != limits.least_y){
   	polygon_unfilled.at<Vec3b>(y, x) = Vec3b(0,0,200);
       }      
     }
@@ -499,7 +507,7 @@ void color_polygon_in_limits(Mat &polygon_unfilled, Limitpoints limits){
 
 
 
-//5.----------------Color the polygon process---------------------------------
+//5.3----------------Color the polygon process---------------------------------
 void color_polygon(Mat &polygon_line, int points[][2], int size_points_array){
   //return leftmost and rightmost x and y
   Limitpoints limits = get_limitpoints_polygon(points,size_points_array);
@@ -507,9 +515,307 @@ void color_polygon(Mat &polygon_line, int points[][2], int size_points_array){
 }
 
 
+//6---------------------------------------------------------polygons operation(union and intersection)-----------------------------------------------------
+
+struct BGR_color{
+  int b;
+  int g;
+  int r;
+};
+
+struct Polygons_info{
+  int number_points_polygon1;
+  int number_points_polygon2;
+  BGR_color bgr_polygon1;
+  BGR_color bgr_polygon2;
+};
+
+
+//6.1 ------------count points of each of 2 polygons to further build an array----------
+Polygons_info count_points_2polygon(string filename){
+
+  //input from file
+  ifstream inFile;
+  inFile.open(filename.c_str());
+  //Check for error
+  if (inFile.fail()){
+    cerr << "count_points_2polygon :: Error Opening File" << endl;
+    exit(1);
+  }
+        
+  //count items
+  string item;
+  int polygon, end_string;
+  int count_bgr;//r = 1, g = 2, b = 3, end = 4
+  Polygons_info polygons_info;
+  polygons_info.number_points_polygon1 = 0;
+  polygons_info.number_points_polygon2 = 0;
+
+  while (!inFile.eof()){
+    inFile >> item;
+    // cout << "item = " << item << endl;
+    if (item == "P1"){
+      polygon = 1;
+      count_bgr = 1 ;
+    }
+    else if (item == "P2"){
+      polygon = 2;
+      count_bgr = 1;
+    }
+    //get info of polygon 1
+    if (item != "P1" && polygon == 1){
+      if(count_bgr == 1){
+	end_string = item.find(",");
+	stringstream convertR1(item.substr(1,end_string));
+	convertR1 >> polygons_info.bgr_polygon1.r; 
+	count_bgr = 2;
+      }
+      else if (count_bgr == 2){
+	end_string = item.find(",");
+	stringstream convertG1(item.substr(1,end_string));
+	convertG1 >> polygons_info.bgr_polygon1.g; 
+	count_bgr = 3;	
+      }
+      else if (count_bgr == 3){
+	end_string = item.find(")");
+	stringstream convertB1(item.substr(0,end_string));
+	convertB1 >> polygons_info.bgr_polygon1.b; 
+	count_bgr = 4;
+      }
+      else if (count_bgr == 4){
+	polygons_info.number_points_polygon1++;
+      }
+    }
+
+    //get info of polygon 2
+    else if (item != "P2" && polygon == 2){
+      if(count_bgr == 1){
+	end_string = item.find(",");
+	stringstream convertR2(item.substr(1,end_string));
+	convertR2 >> polygons_info.bgr_polygon2.r; 
+	count_bgr = 2;
+      }
+      else if (count_bgr == 2){
+	end_string = item.find(",");
+	stringstream convertG2(item.substr(1,end_string));
+	convertG2 >> polygons_info.bgr_polygon2.g; 
+	count_bgr = 3;	
+      }
+      else if (count_bgr == 3){
+	end_string = item.find(")");
+	stringstream convertB2(item.substr(0,end_string));
+	convertB2 >> polygons_info.bgr_polygon2.b; 
+	count_bgr = 4;
+      }
+      else if (count_bgr == 4){
+	polygons_info.number_points_polygon2++;
+      }
+    }
+  }
+  return polygons_info; // for test
+}
+ 
+//6.2 ------------input points into arrays of 2 polygons------------------------------------
+
+void input_point_from_text_2polygons(int input_1[][2],int size_input1,int input_2[][2], int size_input2,string filename){
+
+  //input from file
+  ifstream inFile;
+  inFile.open(filename.c_str());
+  //Check for error
+  if (inFile.fail()){
+    cerr << "input_point_from_text_2polygons :: Error Opening File" << endl;
+    exit(1);
+  }
+
+  int index_apos, index_last_brack;
+  string item;
+
+  // input points into first array
+  for(int i1 = -5; i1 < size_input1; i1++){
+    //prevent the lastline multiple copies
+    inFile >> item; 
+    if(i1 >= 0) {
+      if(item.substr(0,1) == "("){
+	index_apos = item.find(",");
+	index_last_brack = item.find(")");
+	stringstream convert1(item.substr(1,index_apos));
+	convert1 >> input_1[i1][0];
+	stringstream convert2(item.substr(index_apos + 1,index_last_brack));
+	convert2 >> input_1[i1][1];	
+      }
+      // cout << "i1  :  " << input_1[i1][0] << " , " << input_1[i1][1] << endl;
+      // cout << "i1 is " << i1 << endl;
+    }
+  }
+
+  
+  // input points into second array
+  for(int i2 = -4; i2 < size_input2; i2++){
+    //prevent the lastline multiple copies
+    inFile >> item;     
+    // cout << "item is " << item << endl;
+    if(i2 >= 0) {
+      if(item.substr(0,1) == "("){
+	index_apos = item.find(",");
+	index_last_brack = item.find(")");
+	stringstream convert1(item.substr(1,index_apos));
+	convert1 >> input_2[i2][0];
+	stringstream convert2(item.substr(index_apos + 1,index_last_brack));
+	convert2 >> input_2[i2][1];
+      }
+      // cout << "i2  :  " << input_2[i2][0] << " , " << input_2[i2][1] << endl;
+      // cout << "i2 is " << i2 << endl;
+    }      
+  }
+}
+
+
+//6.3------------------
 
 
 
+//6.3-----------------------color a polygon (scan-line) from image to image--------
+void color_scan_line_im_to_im(Mat &image, Mat &back_image, int mat_point[][2],int size_input, int B, int G, int R){
+  //return leftmost and rightmost x and y
+  Limitpoints limits = get_limitpoints_polygon(mat_point, size_input);
+  int fill = 0;
+
+  for(int y = limits.least_y; y < limits.most_y ; y++){
+    //make the vertical sweepline for each step of horizontal sweepline(x-axis with each x-axis)
+    for(int x = limits.least_x; x < limits.most_x ; x++){
+      //if the pixel before is black and now is white, fill = 1, and fill that pixel
+      if((back_image.at<Vec3b>(y, x - 1) == Vec3b(0,0,0)) && (fill == 0)){
+  	fill = 1;
+      }
+      else if((back_image.at<Vec3b>(y,x) == Vec3b(0,0,0)) && (fill == 1)){
+	break;
+      }
+      if(fill == 1 and y != limits.least_y){
+        image.at<Vec3b>(y, x) = Vec3b(B,G,R);
+      }      
+    }
+    fill = 0;
+  }
+}
+
+
+void color_union_scan_line_im_to_im(Mat &image, Mat &back_image, int mat_point[][2],int size_input, int B, int G, int R){
+  //return leftmost and rightmost x and y
+  Limitpoints limits = get_limitpoints_polygon(mat_point, size_input);
+  int fill = 0;
+
+  for(int y = limits.least_y; y < limits.most_y ; y++){
+    //make the vertical sweepline for each step of horizontal sweepline(x-axis with each x-axis)
+    for(int x = limits.least_x; x < limits.most_x ; x++){
+      //if the pixel before is black and now is white, fill = 1, and fill that pixel
+      if((back_image.at<Vec3b>(y, x - 1) == Vec3b(0,0,0)) && (fill == 0)){
+  	fill = 1;
+      }
+      else if((back_image.at<Vec3b>(y,x) == Vec3b(0,0,0)) && (fill == 1)){
+	break;
+      }
+      if(fill == 1 and y != limits.least_y){
+        image.at<Vec3b>(y, x) = Vec3b(B,G,R);
+	if(back_image.at<Vec3b>(y,x + 1) != Vec3b(255,255,255)){
+	  image.at<Vec3b>(y, x + 1) = Vec3b(B,G,R);
+	}
+	if(back_image.at<Vec3b>(y-1,x) == Vec3b(0,0,0) || back_image.at<Vec3b>(y -2,x) != Vec3b(255,255,255)){
+	  image.at<Vec3b>(y-1,x) = Vec3b(B,G,R);
+	}      
+	if(back_image.at<Vec3b>(y+1,x) == Vec3b(0,0,0) || back_image.at<Vec3b>(y +2,x) != Vec3b(255,255,255)){
+	  image.at<Vec3b>(y+1,x) = Vec3b(B,G,R);
+	}      
+      }
+    }
+    fill = 0;
+  }
+}
+
+
+//6.4-----------------------draw and color a polygon--------------------------------
+void draw_color_polygon_from_im_to_im(Mat &image,Mat &back_image, int mat_point[][2], int mat_size, int B, int G, int R, string operation_type){
+
+  //draw polygons in both image
+  draw_polygon_line(image, mat_point, mat_size);
+  draw_polygon_line(back_image, mat_point, mat_size);
+
+  //color the real image according to the polygon image
+  if(operation_type == "i"){
+  color_scan_line_im_to_im(image, back_image, mat_point, mat_size, B, G, R);
+  }
+  else if(operation_type == "u"){
+    color_union_scan_line_im_to_im(image, back_image, mat_point, mat_size, 0, 0, 255);
+  }
+}
+  
+//6.----------------Union and intersect operation-------------------------------------------
+
+void two_polygons_operation(string filename, string operation_type){
+
+    Mat image(1000, 1000, CV_8UC3);//the final image of 2 polygons
+    Mat polygon_1(1000, 1000, CV_8UC3);//first polygon accd to least_y
+    Mat polygon_2(1000, 1000, CV_8UC3);//second polygon according to y then x
+    color_whole_pict(image,255,255,255);
+    color_whole_pict(polygon_1,255,255,255);
+    color_whole_pict(polygon_2,255,255,255);
+
+    //seperate points into two polygons
+    //count number of points of each polygon
+    Polygons_info polygons_info = count_points_2polygon(filename);
+    // cout << "number_points_polygon1 = "<< polygons_info.number_points_polygon1 << endl;
+    // cout << "number_points_polygon2 = "<< polygons_info.number_points_polygon2 << endl;
+    // cout << "polygons_info.bgr_polygon1.r = "<< polygons_info.bgr_polygon1.r << endl;
+    // cout << "polygons_info.bgr_polygon1.g = "<< polygons_info.bgr_polygon1.g << endl;
+    // cout << "polygons_info.bgr_polygon1.b = "<< polygons_info.bgr_polygon1.b << endl;
+    // cout << "polygons_info.bgr_polygon2.r = "<< polygons_info.bgr_polygon2.r << endl;
+    // cout << "polygons_info.bgr_polygon2.g = "<< polygons_info.bgr_polygon2.g << endl;
+    // cout << "polygons_info.bgr_polygon2.b = "<< polygons_info.bgr_polygon2.b << endl;
+
+    // create arrays of both polygon points
+    int input_1[polygons_info.number_points_polygon1][2];
+    int input_2[polygons_info.number_points_polygon2][2];
+    
+    //get input arrays size
+    const int size_input1 = sizeof(input_1)/(2*sizeof(input_1[0][0]));
+    const int size_input2 = sizeof(input_2)/(2*sizeof(input_2[0][0]));
+    cout << "size in 1 = " << size_input1 << endl;
+    cout << "size in 2 = " << size_input2 << endl;
+
+    //input points to array
+    input_point_from_text_2polygons(input_1, size_input1, input_2, size_input2, filename);
+
+    //Determine front and rear polygons according to least x of both
+    Limitpoints limits1 = get_limitpoints_polygon(input_1, size_input1);
+    Limitpoints limits2 = get_limitpoints_polygon(input_2, size_input2);
+    int front;
+    if (limits1.least_x < limits2.least_x){front = 1;}
+    else {front = 2;}
+    
+    //color with scan line according to each own image with the sequence of rear then front
+
+    if(front == 1 && operation_type == "i"){
+      draw_color_polygon_from_im_to_im(image, polygon_2, input_2, size_input2, polygons_info.bgr_polygon1.b, polygons_info.bgr_polygon2.g, polygons_info.bgr_polygon2.r, "i");
+      draw_color_polygon_from_im_to_im(image, polygon_1, input_1, size_input1, polygons_info.bgr_polygon1.b, polygons_info.bgr_polygon1.g, polygons_info.bgr_polygon1.r, "i");
+    }
+    else if(front == 2 && operation_type == "i"){
+      draw_color_polygon_from_im_to_im(image, polygon_1, input_1, size_input1, polygons_info.bgr_polygon1.b, polygons_info.bgr_polygon1.g, polygons_info.bgr_polygon1.r, "i");
+      draw_color_polygon_from_im_to_im(image, polygon_2, input_2, size_input2, polygons_info.bgr_polygon1.b, polygons_info.bgr_polygon2.g, polygons_info.bgr_polygon2.r, "i");
+    }
+
+    if(front == 1 && operation_type == "u"){
+      draw_color_polygon_from_im_to_im(image, polygon_2, input_2, size_input2, 0, 0, 255, "u");
+      draw_color_polygon_from_im_to_im(image, polygon_1, input_1, size_input1, 0, 0, 255, "u");
+    }
+    else if(front == 2 && operation_type == "u"){
+      draw_color_polygon_from_im_to_im(image, polygon_1, input_1, size_input1, 0, 0, 255, "u");
+      draw_color_polygon_from_im_to_im(image, polygon_2, input_2, size_input2, 0, 0, 255, "u");
+    }
+
+    //imshow
+    namedWindow("figure", WINDOW_AUTOSIZE);
+    imshow("figure",image);
+}
 
 
 
@@ -517,40 +823,57 @@ void color_polygon(Mat &polygon_line, int points[][2], int size_points_array){
 
 
 int main(){
-  Mat image(1000, 1000, CV_8UC3); //B,G,R
-  color_whole_pict(image,255,255,255); //color whole pict to be white
+
+  string filename = "Input-2.txt";
+
+  //return the head of the file, "T" or "P"
+  string first_line = get_first_line(filename);
   
-  //input from file
-  ifstream inFile;
-  inFile.open("Input1.txt");
-  //Check for error
-  if (inFile.fail()){
-    cerr << "Error Opening File" << endl;
-    exit(1);
+  //Determine the process from T and P 
+  if(first_line == "P"){
+    
+    Mat image(1000, 1000, CV_8UC3); //B,G,R
+    color_whole_pict(image,255,255,255); //color whole pict to be white
+    
+    //input from file
+    ifstream inFile;
+    inFile.open(filename.c_str());
+    //Check for error
+    if (inFile.fail()){
+      cerr << "Error Opening File" << endl;
+      exit(1);
+    }
+        
+    //count items
+    string item;
+    int count = 0;
+    while (!inFile.eof()){
+      inFile >> item;
+      count++;
+    }
+
+    //create a list of points
+    int input[count - 2][2];
+    const int size_input = sizeof(input)/(2*sizeof(input[0][0]));
+    //get input points
+    input_point_from_text(input,filename, size_input);
+    
+    
+    //draw polygon by connecting points
+    draw_polygon_line(image,input, size_input);
+    
+    //color the polygon
+    color_polygon(image,input,size_input);
+
+    namedWindow("figure", WINDOW_AUTOSIZE);
+    imshow("figure",image);
+
+
+  }//Closed bracket for P function
+  else if(first_line == "T"){
+
+    two_polygons_operation(filename, "u");
   }
-  //count items
-  string item;
-  int count = 0;
-  while (!inFile.eof()){
-    inFile >> item;
-    count++;
-  }
-  //create a list of points
-  int input[count - 2][2];
-  const int size_input = sizeof(input)/(2*sizeof(input[0][0]));
-  //get input points
-  input_point_from_text(input,"Input1.txt", size_input);
-
-
-  //draw polygon by connecting points
-  draw_polygon_line(image,input, size_input);
-  
-  //color the polygon
-  color_polygon(image,input,size_input);
-
-
-  namedWindow("figure", WINDOW_AUTOSIZE);
-  imshow("figure",image);
   waitKey(0);
   return 0;
 }
